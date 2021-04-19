af88bd07535006c5f0111df969f8f6e763ac51ce
Chelsey <chelsey.tschida@outlook.com>
2021-04-18 22:03:19 -0400

    protected views

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
-import Auth from "./components/Auth/Auth"
+import Auth from './components/Auth/Auth'
 import Navbar from './components/Site/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 type valueTypes = {
   token: any
@@ -11,38 +12,42 @@ class App extends Component<{}, valueTypes> {
   constructor(props: valueTypes) {
     super(props)
     this.state = {
-      token: ""
+      token: '',
     }
-}
-
-componentDidMount() {
-  if(localStorage.getItem("sessionToken")) {
-    this.setState({
-      token: localStorage.getItem("sessionToken")
-    })
   }
-}
 
-updateToken = (newToken: any) => {
-  localStorage.setItem("sessionToken", newToken)
-  this.setState({ token: newToken})
-  console.log("is this updating the token", this.state.token)
-}
+  componentDidMount() {
+    if (localStorage.getItem('sessionToken')) {
+      this.setState({
+        token: localStorage.getItem('sessionToken'),
+      })
+    }
+  }
 
-clearToken = () => {
-  localStorage.clear();
-  this.setState({ token: '' })
-  console.log('token cleared')
-}
+  updateToken = (newToken: any) => {
+    localStorage.setItem('sessionToken', newToken)
+    this.setState({ token: newToken })
+    console.log('is this updating the token', this.state.token)
+  }
 
+  clearToken = () => {
+    localStorage.clear()
+    this.setState({ token: '' })
+    console.log('token cleared')
+  }
+
+  protectedViews = () => {
+    return this.state.token === localStorage.getItem('sessionToken') ? (
+      <Router>
+        <Navbar logout={this.clearToken} token={this.state.token} />
+      </Router>
+    ) : (
+      <Auth token={this.updateToken} />
+    )
+  }
 
   render() {
-    return (
-      <div className="App">
-        <Navbar logout={this.clearToken} token={this.updateToken} />
-        <Auth token={this.updateToken} />
-      </div>
-    )
+    return <div className='App'>{this.protectedViews()}</div>
   }
 }
 
--- src/components/Site/Navbar.tsx
+++ src/components/Site/Navbar.tsx
@@ -1,29 +1,43 @@
 import React, { Component } from 'react'
+import { Link, Route, Switch } from 'react-router-dom'
+import Home from './Home'
 
 type acceptedProps = {
-    token: any
-    logout: any
+  token: any
+  logout: any
 }
 
 export default class Navbar extends Component<acceptedProps, {}> {
-    constructor(props: acceptedProps) {
-        super(props)
-        this.state = {
+  constructor(props: acceptedProps) {
+    super(props)
+    this.state = {}
+  }
 
-        }
-    }
+  logoutButton = () => {
+    return localStorage.getItem('sessionToken') === null ? (
+      ''
+    ) : (
+      <button onClick={this.props.logout}>Logout</button>
+    )
+  }
 
-    logoutButton = () => {
-        return localStorage.getItem('sessionToken') === null
-        ? ''
-        : <button onClick={this.props.logout}>Logout</button>
-    }
-
-    render() {
-        return(
-            <div>
-                {this.logoutButton()}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <nav className='bg-red-400 h-12'>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>{this.logoutButton()}</li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/Home'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    )
+  }
 }
